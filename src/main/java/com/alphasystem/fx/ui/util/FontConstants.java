3c2f44e5a2627275c78ccd9475459496db08fe86
sfali23 <f.syed.ali@gmail.com>
2017-01-02 19:32:26 -0500

    updated

--- src/main/java/com/alphasystem/fx/ui/util/FontConstants.java
+++ src/main/java/com/alphasystem/fx/ui/util/FontConstants.java
@@ -15,10 +15,16 @@ public interface FontConstants {
 
     String ARABIC_FONT_NAME_KEY = "arabic.font.name";
     String ENGLISH_FONT_NAME_KEY = "english.font.name";
+    String ARABIC_NORMAL_FONT_SIZE_KEY = "arabic.normal.font.size";
+    String ARABIC_HEADING_FONT_SIZE_KEY = "arabic.heading.font.size";
     String ARABIC_FONT_NAME = getProperty(ARABIC_FONT_NAME_KEY, "Arabic Typesetting");
     String ENGLISH_FONT_NAME = getProperty(ENGLISH_FONT_NAME_KEY, "Candara");
+    long ARABIC_NORMAL_FONT_SIZE = Long.getLong(ARABIC_NORMAL_FONT_SIZE_KEY, 32);
+    long ARABIC_HEADING_FONT_SIZE = Long.getLong(ARABIC_HEADING_FONT_SIZE_KEY, 72);
 
     // Arabic Fonts
+    Font ARABIC_NORMAL_FONT = font(ARABIC_FONT_NAME, BLACK, REGULAR, ARABIC_NORMAL_FONT_SIZE);
+    Font ARABIC_HEADING_FONT = font(ARABIC_FONT_NAME, BLACK, REGULAR, ARABIC_HEADING_FONT_SIZE);
     Font ARABIC_FONT_48 = font(ARABIC_FONT_NAME, BLACK, REGULAR, 48.0);
     Font ARABIC_FONT_36 = font(ARABIC_FONT_NAME, BLACK, REGULAR, 36.0);
     Font ARABIC_FONT_30 = font(ARABIC_FONT_NAME, BLACK, REGULAR, 30);
